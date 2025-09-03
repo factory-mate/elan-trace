5e713395b5a30a0fc7bd6baf9b6c352211e92b89
Bruce <recall4056@gmail.com>
2025-09-04 02:25:52 +0800

    fix: remove border

--- src/pages/index/components/sunshine-factory.vue
+++ src/pages/index/components/sunshine-factory.vue
@@ -6,6 +6,9 @@
       src="http://139.224.82.135:9990/#/photos"
       width="100%"
       height="250px"
+      border="0"
+      frameborder="no"
+      framespacing="0"
     />
   </view>
 </template>
